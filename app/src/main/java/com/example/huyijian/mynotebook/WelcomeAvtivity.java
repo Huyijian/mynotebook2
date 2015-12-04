ef5a132bfd3ec1affd61b4c1627cbefb3e3e2f0a
Huyijian <huyijian@huyijiandeMacBook-Pro.local>
2015-12-04 18:11:07 +0800

    记笔记

---
+++ app/src/main/java/com/example/huyijian/mynotebook/WelcomeAvtivity.java
@@ -0,0 +1,18 @@
+package com.example.huyijian.mynotebook;
+
+/**
+ * Created by huyijian on 15/12/4.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeAvtivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+
+}
